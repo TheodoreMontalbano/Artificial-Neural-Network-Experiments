306405a404bb951662247a87dab2fcc19d758501
Theodore Montalbano <tmontalbano@wisc.edu>
2022-11-29 21:06:36 -0600

    Creating Interfaces necessary for Game + Math Functions +
    NN + Player

---
+++ GameInterfaces/IArrayGame.py
@@ -0,0 +1,21 @@
+class IArrayGame:
+    # Current state of the game
+    state = None
+    players = None
+    currPlayer = None
+
+    # The current player makes a move
+    def makeMove(self):
+        pass
+
+    # Has the players play a simulation of the game
+    def playGame(self):
+        pass
+
+    # Checks if the game is over
+    def gameState(self):
+        pass
+
+    # Outputs the gamestate to the viewer
+    def show(self):
+        pass
---
+++ GameInterfaces/IPlayer.py
@@ -0,0 +1,10 @@
+class IPlayer:
+
+    # If this is an AI return true
+    # if this is a player don't
+    def isRobot(self):
+        pass
+
+    # queries player for a move based on state
+    def makeMove(self, state):
+        pass
